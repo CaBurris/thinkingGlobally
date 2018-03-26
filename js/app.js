58636aa09b08ba25195168e8f083deeee080df78
Christy <caburris@ucdavis.edu>
2018-03-26 14:07:13 -0700

    added error-not-purple class

--- js/app.js
+++ js/app.js
@@ -1,8 +1,23 @@
 
-const myHeading = document.getElementById("myHeading");
+/* NOTES---
+const myHeading = document.getElementsByTagName("h1")[0];
 const myButton = document.getElementById("myButton");
 const myTextInput = document.getElementById("myTextInput");
 
 myButton.addEventListener('click', () => {
     myHeading.style.color = myTextInput.value;
 });
+
+*/
+
+const myList = document.getElementsByTagName('li');
+
+for (let i = 0; i < myList.length; i += 1) {
+    myList[i].style.color = 'purple';
+}
+
+const errorNotPurple = document.getElementsByClassName('error-not-purple');
+
+for (let i = 0; i < errorNotPurple.length; i += 1) {
+    errorNotPurple[i].style.color = 'red';
+}
